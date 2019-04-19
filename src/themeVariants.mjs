8393b365f2fa2b24b5a3057f638fbab8933f7d06
Juliette Pretot <hi@juliette.sh>
2019-04-19 18:55:57 +0200

    add warm variant

--- src/themeVariants.mjs
+++ src/themeVariants.mjs
@@ -1,9 +1,24 @@
+import tinycolor from 'tinycolor2'
+import colorBlend from 'color-blend'
+
 const THEME_VARIANTS = [
   {
     label: 'lucy',
     uiTheme: 'vs-dark',
     path: './dist/lucy-theme.json',
     filter: colorCode => colorCode
+  },
+  {
+    label: 'lucy warm',
+    uiTheme: 'vs-dark',
+    path: './dist/lucy-warm-theme.json',
+    filter: colorCode =>
+      tinycolor(
+        colorBlend.softLight(
+          tinycolor(colorCode).toRgb(),
+          tinycolor('red').toRgb()
+        )
+      ).toHexString()
   }
 ]
 
